0a48ca1db594234c815f55f2b09271aad9f043e5
Luisss003 <145069480+Luisss003@users.noreply.github.com>
2025-07-13 22:58:20 -0500

    Created model for analysis task

--- analysis/models.py
+++ analysis/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from samples.models import Sample
 
 # Create your models here.
+
+class AnalysisJob(models.Model):
+    sample = models.ForeignKey(Sample, on_delete=models.CASCADE)
+    started_at = models.DateTimeField(auto_now_add=True)
+    finished_at = models.DateTimeField(null=True, blank=True)
+    success = models.BooleanField(default=False)
+    log = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Job for {self.sample.filename} started at {self.started_at}"
