53bff50868d3be048f186fe3db382bdeea8ac886
Luisss003 <145069480+Luisss003@users.noreply.github.com>
2025-07-23 22:01:51 -0500

    Created project in Spring

--- src/main/java/com/luisss003/luthonbox/controller/AnalysisController.java
+++ src/main/java/com/luisss003/luthonbox/controller/AnalysisController.java
@@ -1,4 +1,9 @@
 package com.luisss003.luthonbox.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
 public class AnalysisController {
 }
