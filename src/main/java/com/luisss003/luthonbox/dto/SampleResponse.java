7d2a383c7fecb37ecd76de61ffadc926d55be102
Luisss003 <145069480+Luisss003@users.noreply.github.com>
2025-07-23 22:20:58 -0500

    Added constructor to SampleResponse

--- src/main/java/com/luisss003/luthonbox/dto/SampleResponse.java
+++ src/main/java/com/luisss003/luthonbox/dto/SampleResponse.java
@@ -3,4 +3,7 @@ package com.luisss003.luthonbox.dto;
 public class SampleResponse {
     private String result;
     private String reportUrl;
+
+    public SampleResponse(String analysisResult, Object o) {
+    }
 }
