4afeb86c21d7f32a4433ea75a00bb62c3d054588
Luisss003 <145069480+Luisss003@users.noreply.github.com>
2025-07-23 22:18:52 -0500

    Created POST endpoint for /api/analysis

--- src/main/java/com/luisss003/luthonbox/controller/AnalysisController.java
+++ src/main/java/com/luisss003/luthonbox/controller/AnalysisController.java
@@ -1,9 +1,28 @@
 package com.luisss003.luthonbox.controller;
 
+import com.luisss003.luthonbox.dto.SampleRequest;
+import com.luisss003.luthonbox.dto.SampleResponse;
+import com.luisss003.luthonbox.model.Sample;
+import com.luisss003.luthonbox.service.AnalysisService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/analyses")
 public class AnalysisController {
+
+    private final AnalysisService analysisService;
+
+    public AnalysisController(AnalysisService analysisService) {
+        this.analysisService = analysisService;
+    }
+
+    @PostMapping
+    public ResponseEntity<SampleResponse> analyze(@RequestBody SampleRequest request) {
+        Sample sample = analysisService.analyze(request);
+        return ResponseEntity.ok(new SampleResponse(sample.getAnalysisResult(), null));
+    }
 }
